24762d427422f2a6f5f6c3c98c7a81082cebf753
梁亮亮 <liangliangliang@luojilab.com>
2021-11-01 16:13:15 +0800

    update var_const

--- var_const.go
+++ var_const.go
@@ -1,8 +1,7 @@
 package lfc
 
 const (
-	redis_key_prefix        = "fc" //redis key prefix
-	redis_key_shadow_suffix = "_shadow"
-	redis_key_split         = ":" //redis key delimiter
-	redis_key_inner_split   = "#" //redis key delimiter : fc:1:uid#device_id:86400:[uid's value]#[device's value]
+	redis_key_prefix      = "fc" //redis key prefix
+	redis_key_split       = ":"  //redis key delimiter
+	redis_key_inner_split = "#"  //redis key delimiter :
 )
